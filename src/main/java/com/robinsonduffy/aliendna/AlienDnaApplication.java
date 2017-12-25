cab26a7b5df462636f1c97a97f64f3d912f91cdf
Robinson Duffy <rduffy@ebates.com>
2017-12-24 21:14:46 -0600

    Initial commit

---
+++ src/main/java/com/robinsonduffy/aliendna/AlienDnaApplication.java
@@ -0,0 +1,12 @@
+package com.robinsonduffy.aliendna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlienDnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlienDnaApplication.class, args);
+	}
+}
