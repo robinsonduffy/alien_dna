e9f0ace5b022f6df4cef10a534ccc757105518b1
Robinson Duffy <rduffy@ebates.com>
2018-01-11 21:51:08 -0600

    Phenotypes: Ruff/ no ruff

--- src/main/java/com/robinsonduffy/aliendna/model/Markers.java
+++ src/main/java/com/robinsonduffy/aliendna/model/Markers.java
@@ -2,4 +2,5 @@ package com.robinsonduffy.aliendna.model;
 
 public final class Markers {
     public static final String FANGS = "A";
+    public static final String RUFF = "B";
 }
---
+++ src/main/java/com/robinsonduffy/aliendna/model/phenotype/Ruff.java
@@ -0,0 +1,28 @@
+package com.robinsonduffy.aliendna.model.phenotype;
+
+import com.robinsonduffy.aliendna.model.Allele;
+import com.robinsonduffy.aliendna.model.Markers;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Ruff extends AbstractPhenotype{
+    @Override
+    public String getName() {
+        return "Ruff";
+    }
+
+    @Override
+    public String getDetails() {
+        return "Has a ruff around the neck";
+    }
+
+    @Override
+    public List<Pair<String, Allele>> getExpression() {
+        return Arrays.asList(
+            new ImmutablePair<>(Markers.RUFF, Allele.DOMINANT)
+        );
+    }
+}
