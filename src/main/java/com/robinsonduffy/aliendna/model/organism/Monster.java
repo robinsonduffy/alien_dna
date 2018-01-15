e9d18785a22099eace028382a1f28b7821498305
Robinson Duffy <rduffy@ebates.com>
2018-01-15 16:05:17 -0600

    MonsterResolver with autowired phenotypes TODO: Finish implementing all the MonsterPhenotypes

---
+++ src/main/java/com/robinsonduffy/aliendna/model/organism/Monster.java
@@ -0,0 +1,13 @@
+package com.robinsonduffy.aliendna.model.organism;
+
+import com.robinsonduffy.aliendna.model.genotype.Sequence;
+import com.robinsonduffy.aliendna.model.phenotype.Phenotype;
+
+import java.util.List;
+
+public class Monster extends AbstractOrganism {
+
+    public Monster(Sequence sequence) {
+        super(sequence);
+    }
+}
